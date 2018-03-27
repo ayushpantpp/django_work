07d013877fc9587e99e10d86ee0ba43236062e28
ayushpantpp <ayush.pant@essindia.com>
2018-03-28 00:01:39 +0530

    login Created

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     url(r'^index$', views.index),
-    url(r'^login$', views.login),
+    #url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'app/login.html'}, name='logout'),
+    url(r'^login/$', auth_views.login, {'template_name': 'app/login.html'}, name='login'),
+
+    #url(r'^login$', views.login),
     url(r'^register$', views.register),
     url(r'^contact_us$', views.contact_us),
     url(r'^(?P<album_id>[0-9]+)$', views.detail, name="details"),
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404
+
 from .models import Album
 from django.shortcuts import render, get_object_or_404
 
